0f91535f6793dbc4d43fb8b3ac30797085531476
Palita Sukthae <palita311999@gmail.com>
2026-02-16 13:10:38 +0700

    Clean component unused

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import EditeView from '@/views/EditeView.vue'
 
 
 const router = createRouter({
@@ -16,11 +15,6 @@ const router = createRouter({
       path: '/todo/:id',
       name: 'todo-item',
       component: HomeView
-    },
-    {
-      path: '/todo/edit/:id',
-      name: 'todo-edit',
-      component: EditeView
     }
   ]
 })
